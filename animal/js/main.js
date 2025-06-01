fc4ae7feaa319452e316ab10f855a831d783b309
jae-K11 <jojo2852@naver.com>
2025-06-01 23:30:04 +0900

    .

--- animal/js/main.js
+++ animal/js/main.js
@@ -1,5 +1,103 @@
 $(document).ready(function(){
-    //console.log('나와?')
+/********************* header의 메뉴 : 시작 *************
+ * pc인지 모바일인지 구분 - 브라우저 넓이로
+ * 스크롤값계산
+ * 공통사항 : 브라우저가 스크롤되면 or header에 오버하면  header에  fixed클래스 추가
+ * pc일때 : 마우스를 오버한  li에만  over 클래스 추가 
+ * 모바일떄 : 메뉴열기를 클릭하면   header 에 menu_open 클래스 추가
+ *           1차메뉴를 클릭하면 (하위메뉴가 있는  1차메뉴만 ) 클릭한 li에  open클래스축가
+ *********/
+
+let device_status //모바일인지 pc인지
+let scrolling //스크롤한 값 
+let  window_w //브라우저 넓이
+let  mobile_size = 1024 //모바일로 전환되는 사이즈 
+
+scroll_chk()//함수실행 (처음에 문서가 로딩되었을때 1번)
+resize_chk() //함수실행
+$(window).resize(function(){//브라우저가 리사이즈될떄마다 1번씩 실행
+    resize_chk() //함수실행
+})
+$(window).scroll(function(){
+    scroll_chk()//함수실행
+})
+
+function scroll_chk(){
+    //console.log('스크롤했다')
+    scrolling = $(window).scrollTop()
+    //console.log(scrolling)
+    if(scrolling > 0){
+        $('header').addClass('fixed')
+    }else{
+        $('header').removeClass('fixed')
+    }
+}
+function resize_chk(){
+    //console.log('리사이즈됬다')
+    window_w = $(window).width()
+    if(window_w > mobile_size){
+        device_status = 'pc'
+    }else{ 
+        device_status = 'mobile'
+    }
+    //console.log(device_status)
+}
+
+/* header에 마우스를 오버했을때 -- 클릭했을떄도 작동함*/
+$('header').on('mouseenter', function(){
+    if(device_status == 'pc'){
+    $('header').addClass('fixed')
+    console.log('너?')
+    }
+})
+$('header').on('mouseleave', function(){
+    /* <= 작거나 같으면 , 브라우저가 스크롤 된 상태에서는 header 가 fixed 클래스를 삭제하면 안됨
+    맨 위에 있을때만 삭제해야함*/  
+    if(scrolling <= 0){      
+        //console.log('지금은 아니야')
+        console.log('아웃')
+        $('header').removeClass('fixed')
+    }//if 종료
+    
+})
+
+$('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter', function(){
+    if(device_status == 'pc'){
+      //  console.log('오버했따')
+        $(this).addClass('over')
+    }
+}) 
+$('header .gnb .gnb_wrap ul.depth1 > li').on('mouseleave', function(){
+   // console.log('마우스아웃')
+    $(this).removeClass('over')
+}) 
+
+$('header .gnb .gnb_open').on('click', function(){
+    $('header').addClass('menu_open')
+})
+$('header .gnb .gnb_close').on('click', function(){
+    $('header').removeClass('menu_open')
+})
+/*
+    닫힌메뉴를 클릭하면 열리고 , 열린메뉴를 클릭하면 닫힘
+    동시에 여러개의 메뉴가 열려있을 수도 있음
+    toggleClass - 클래스가 없으면 추가하고 있으면 삭제
+*/
+
+
+$('header .gnb .gnb_wrap ul.depth1 > li:has(ul.depth2) > a').on('click', function(e){
+    if(device_status == 'mobile'){
+         e.preventDefault()
+         console.log('클릭했다')
+         $(this).parents('li').toggleClass('open')
+    }
+   
+})
+
+/********************* header의 메뉴 : 종료 *********************/
+
+
+ /********************* visual swiper : 시작 *********************/
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
         autoplay: {  /* 팝업 자동 실행 */
@@ -40,6 +138,7 @@ $(document).ready(function(){
         visual_swiper.autoplay.start();  /* 재생 기능 */
         $(this).hide()// 재생버튼 자신은 숨김
         $('.visual .btn_wrap button.btn_stop').show()//정지버튼 나타냄
-
     })
+
+    /********************* visual swiper : 시작 *********************/
 })
