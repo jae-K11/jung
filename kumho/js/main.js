642470aa397f3527dfc0e0c7d8dc77746f88ec86
jae-K11 <jojo2852@naver.com>
2025-06-20 00:31:47 +0900

    .

--- kumho/js/main.js
+++ kumho/js/main.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
     
     /******************visual swiper 연결 : 시작 *********************/
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
-	// autoplay: {  /* 팝업 자동 실행 */
-	// 	delay: 2500,
-	// 	disableOnInteraction: true,
-	// },
+	autoplay: {  /* 팝업 자동 실행 */
+		delay: 5000,
+		disableOnInteraction: true,
+	},
 
 	//effect: "fade", /* fade 효과 */
 
